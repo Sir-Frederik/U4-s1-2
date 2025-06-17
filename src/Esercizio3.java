928a9cc9964cb31aa9af479bb2bf5042bcabd6f0
Federico <brunetti.federico.13@gmail.com>
2025-06-17 18:00:20 +0200

    esercizio 3

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class Esercizio3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ciao, dammi una stringa");
-        String str = scanner.nextLine();
-        while (str != ".q"){
-            str.split("");
-            System.out.println(str);
-            break;
-        }
+        String str ="";
+       do {
+           System.out.println("Ciao, dammi una stringa");
+
+         str = scanner.nextLine();
+           str.split("");
+           System.out.println(str);
+           System.out.println(" Dammene un'altra ");
+           str = scanner.nextLine();
+           break;
+       }
+
+        while (!str.equals(":q") );
+
+
     }
 }
