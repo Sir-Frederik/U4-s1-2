b266e4b5fb30898fb6c6548c4d5a85374e2d4498
Federico <brunetti.federico.13@gmail.com>
2025-06-17 17:07:20 +0200

    es 1 complicato

--- src/Esercizio1.java
+++ src/Esercizio1.java
@@ -10,14 +10,26 @@ public class Esercizio1 {
         return false;
  }
  }
+ public static Boolean  annoBisestile (int anno) {
+ if  (anno % 4 ==0 || (anno % 100 == 0) && (anno  % 400 == 0 )) {
+     return true;
+
+ } else {
+     return false;
+ }
+ }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println( "Inserisci una stringa di testo");
+       /* System.out.println( "Inserisci una stringa di testo");
         String str = scanner.nextLine();
 
        boolean risultato = stringaPariDispari(str);
-        System.out.println( "il numero di caratteri è pari? " + risultato);
+        System.out.println( "il numero di caratteri è pari? " + risultato); */
+
+        System.out.println( "Inserisci l'anno" );
+        int anno = Integer.parseInt(scanner.nextLine());    boolean risultato = annoBisestile(anno);
+        System.out.println( "L'anno è bisesto? " + risultato);
 
     }
 }
